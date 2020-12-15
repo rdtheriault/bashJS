a4e9f231572c1cfb7124bba5d7eecd892af11b05
Robert <rdtheriault@yahoo.com>
2020-12-15 13:18:13 -0800

    Update fileSystem.js
    added files

--- fileSystem.js
+++ fileSystem.js
@@ -5,9 +5,15 @@ var dirs = [
   "/home/admin/documents",
   "/home/admin/documents/passwords.txt",
   "/home/admin/desktop",
+  "/home/admin/desktop/memo.txt",
   "/home/user",
   "/home/user/documents",
   "/home/user/desktop",
   "/bin",
   "/dev"
 ];
+
+var files = {
+  "/home/admin/documents/passwords.txt": "Chris,passsword\nTed,badTed",
+  "/home/admin/desktop/memo.txt": "Go do this"
+};
