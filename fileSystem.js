f80755000d3654b3b9df59e55aac827ab754edb9
Robert <rdtheriault@yahoo.com>
2020-12-15 07:52:02 -0800

    Create fileSystem.js

---
+++ fileSystem.js
@@ -0,0 +1,13 @@
+var dirs = [
+  "/",
+  "/home",
+  "/home/admin",
+  "/home/admin/documents",
+  "/home/admin/documents/passwords.txt",
+  "/home/admin/desktop",
+  "/home/user",
+  "/home/user/documents",
+  "/home/user/desktop",
+  "/bin",
+  "/dev"
+];
